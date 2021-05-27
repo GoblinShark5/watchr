d2454d771989d732573f28fcb4990619e4d1063c
mbromet <mbromet>
2021-05-27 17:59:25 -0500

    got react router working

--- client/MainContainer.jsx
+++ client/MainContainer.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from './components/HomePage.jsx';
 
 import FavoritesView from './views/FavoritesView.jsx';
 import HomeView from './views/HomeView.jsx';
-import LoginView from './views/LoginView.jsx';
+import LoginView from './views/LogInView.jsx';
 import SignUpView from './views/SignUpView.jsx';
 
 import './components/styles/MainContainer.css';
@@ -18,15 +18,15 @@ class MainContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current: 'signup',
+      //current: 'signup',
       loggedIn: false
     };
-    this.views = {
-      'login': <LoginView />,
-      'signup': <SignUpView />,
-      'home': <HomePage />,
-      'favorites': <FavoritesView />
-    };
+    // this.views = {
+    //   'login': <LoginView />,
+    //   'signup': <SignUpView />,
+    //   'home': <HomePage />,
+    //   'favorites': <FavoritesView />
+    // };
   }
 
   toggleCurrent = (value) => {
@@ -36,11 +36,25 @@ class MainContainer extends React.Component {
   render() {
     return (
       <div>
-        {/*-- NavBar here --*/}
-        <button onClick={() => this.toggleCurrent('login')} >Log In</button>
+        {/* -- NavBar here -- */}
+        {/* <button onClick={() => this.toggleCurrent('login')} >Log In</button>
         <button onClick={() => this.toggleCurrent('signup')} >Sign Up</button>
         <button onClick={() => this.toggleCurrent('home')} >Home</button>
-        {this.views[this.state.current]}
+        {this.views[this.state.current]} */}
+
+        <Router>
+          <div>
+            <Route path="/" component={LoginView} exact />
+            <nav>
+              <Link to="/login">Log In</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/home">Home</Link>
+            </nav>
+            <Route path="/login" component={LoginView} />
+            <Route path="/signup" component={SignUpView} />
+            <Route path="/home" component={HomePage} />
+          </div>
+        </Router>
       </div>
     );
   }
